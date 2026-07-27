543b288af4fd1b24a221df499fccafcf5a5738f3
ShreyaMV <shreyamiriyamv@gmail.com>
2026-07-27 22:09:07 +0400

    Day 6 Functions

---
+++ Day 6/Functions.py
@@ -0,0 +1,12 @@
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+print("\n")
+
+def get_your_name():
+    name = input("Enter your name: ")
+    print(f"Hello, {name}")
+
+print("Hello")
+get_your_name()
